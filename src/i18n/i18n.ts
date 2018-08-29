d0e13c59834c9c8dc9e78c5c9713ebce064168d0
RubaXa <ibnRubaXa@gmail.com>
2018-08-29 14:38:08 +0300

    * typings

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -22,4 +22,10 @@ function i18n(phrase: string, parts: any[]) {
 
 i18n['jsx'] = jsxFactory(createElement);
 
-export default i18n
+export default i18n as {
+	(pharse: string, parts: any[]): string;
+	jsx: {
+		(pharse: string, parts: any[]): any;
+		useWrapper: (Cmp: Function) => void;
+	};
+};
